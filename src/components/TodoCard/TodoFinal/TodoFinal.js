eeac5e86bedd920bcfc1f758f6a3a4f9feef42b3
robadrian_va <robadrian.va@gmail.com>
2022-08-10 23:13:34 +0300

    UI update

--- src/components/TodoCard/TodoFinal/TodoFinal.js
+++ src/components/TodoCard/TodoFinal/TodoFinal.js
@@ -1,29 +1,41 @@
-import './TodoFinal.css'
-import { IoIosTrash, IoMdCheckmark, IoMdCreate, IoIosArrowForward } from "react-icons/io";
+import "./TodoFinal.css";
+import {
+  IoIosTrash,
+  IoMdCheckmark,
+  IoMdCreate,
+  IoIosArrowForward,
+} from "react-icons/io";
 import useToggle from "../../../hooks/useToggle";
 
 const TodoFinal = (props) => {
+  const [completed, setToCompleted] = useToggle();
 
-    const [completed, setToCompleted] = useToggle();
+  const onClickHandler = () => {
+    props.delete(props.id);
+  };
 
-    const onClickHandler = () => {
-        props.delete(props.id);
-    }
-
-    return (
-        <>
-        <span className='card'>
-            <p className={!completed ? "Todo" : `TodoDone`}>{props.todo}</p>
-            <p className='date'>{props.date}</p>
-        </span>
-        <div className="icons">
-            {!completed && <IoMdCheckmark className="icon" onClick={setToCompleted}/> }
-            {!completed && <IoMdCreate className="icon" onClick={props.isEditing}/>} 
-            {completed && <IoIosTrash className="icon" onClick={onClickHandler} /> }
-            {completed && <IoIosArrowForward className="icon" onClick={setToCompleted}/> }
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <span className="card">
+        <p className={!completed ? "Todo" : `TodoDone`}>{props.todo}</p>
+        <p className="date">{props.date}</p>
+      </span>
+      <div className="icons">
+        {!completed && (
+          <IoMdCheckmark className="icon check" onClick={setToCompleted} />
+        )}
+        {!completed && (
+          <IoMdCreate className="icon pencil" onClick={props.isEditing} />
+        )}
+        {completed && (
+          <IoIosTrash className="icon delete" onClick={onClickHandler} />
+        )}
+        {completed && (
+          <IoIosArrowForward className="icon arrow" onClick={setToCompleted} />
+        )}
+      </div>
+    </>
+  );
+};
 
 export default TodoFinal;
